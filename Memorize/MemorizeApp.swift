38e927f1c156a8bb5aa5553a86beb388a2a0db34
Miguel Quispe <dev@miguelquispe.com>
2024-02-21 06:01:51 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Miguel Quispe on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
